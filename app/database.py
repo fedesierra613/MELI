e67a4c2e67d74656a136b21195be08fe6189d474
Federico Sierra <86080354+fedesierra613@users.noreply.github.com>
2024-11-12 01:43:33 -0500

    Cambios a database

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
 import os
 import redis
-from dotenv import load_dotenv
-
-# Carga variables de entorno desde un archivo .env en modo de desarrollo
-load_dotenv()
 
 # Obtiene la URL de Redis desde el entorno, usa la configuración local si no está definida
 redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+# Establece la conexión con Redis utilizando la URL obtenida
 redis_client = redis.StrictRedis.from_url(redis_url, decode_responses=True)
